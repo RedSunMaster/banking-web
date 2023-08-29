c687c1c4888e649ee87b8d1b5e2a02b61d9e0a14
RedSunMaster <richard.mcnulty20@gmail.com>
2023-08-29 16:28:45 +1200

    added transasctions page - pagination - filtering - sorting  themes (WIP)

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,25 @@ import './utils/theme.css';
 
 import { NavBar } from './components/navbar';
 import { Login } from './pages/login';
+import { Transactions } from './pages/transactions';
 import { Balances } from './pages/balances'
 import Cookies from 'js-cookie';
 import axios from 'axios';
+import {
+  createTheme,
+  ThemeProvider
+} from "@mui/material/styles";
+
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark'
+  },
+  typography: {
+    fontSize: 14
+  },
+});
+
 
 async function checkIsLoggedIn(): Promise<boolean> {
   try {
@@ -33,8 +49,8 @@ function App() {
   }, []);
 
   return (
+    <ThemeProvider theme={theme}>
     <div>
-      
         <Router>
           <NavBar isLoggedIn={isLoggedIn} />
           <div className='content'>
@@ -42,13 +58,14 @@ function App() {
           
             <Route path="/login" element={<Login />} />
             <Route path="/balances" element={<Balances />} />
-            <Route path="/transactions" element={<></>} />
+            <Route path="/transactions" element={<Transactions />} />
             <Route path="/money-owed" element={<></>} />
           
           </Routes>
           </div>
         </Router>
       </div>
+    </ThemeProvider>
   );
 }
 
--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -1,6 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Cookies from 'js-cookie';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
+import { DataGrid, GridColDef, GridToolbar } from '@mui/x-data-grid';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Cookies from 'js-cookie'
 
+
+interface TransactionItem {
+    transactionID: number;
+    userId: number;
+    Date: Date;
+    Amount: number;
+    Category: string;
+    Description: string;
+    Transaction: string;
+  }
+  
+
+export const Transactions = () => {
+  const [transactions, setTransactions] = useState<TransactionItem[]>([]);
+  
+  useEffect(() => {
+    const fetchTransactions = async () => {
+        const authToken = Cookies.get('authToken');
+        const response = await axios.get('/api/transactions', {
+          headers: { Authorization: `Bearer ${authToken}` },
+        });
+        const formattedData = response.data.map((transaction: TransactionItem) => ({
+          ...transaction,
+          Date: new Date(transaction.Date).toLocaleDateString(),
+        }));
+        setTransactions(formattedData);
+      };      
+    fetchTransactions();
+  }, []);
+
+  const columns: GridColDef[] = [
+    { field: 'transactionID', headerName: 'ID', flex: 1 },
+    { field: 'Date', headerName: 'Date', flex: 1 },
+    { field: 'Amount', headerName: 'Amount', flex: 1, renderCell: (params) => `$${params.value}`},
+    { field: 'Category', headerName: 'Category', flex: 1 },
+    { field: 'Description', headerName: 'Description', flex: 1 },
+  ];
+  
+  return (
+    <>
+      <div style={{ height: 600, width: '100%' }}>
+        <DataGrid
+        getRowId={(row) => `${row.transactionID}`}
+        rows={transactions}
+        columns={columns}
+        slots={{
+            toolbar: GridToolbar,
+        }}
+        />
+
+      </div>
+    </>
+  );
+};
+
+export default Transactions;
