34a590ebbd1928e1c028cd9301f2c37529cc7d3f
RedSunMaster <richard.mcnulty20@gmail.com>
2023-09-06 15:35:36 +1200

    Big Update
    - Redesigned calling from database (now can do it in chunks)
    - pages only requests data it needs
    - pages only update data it has changed
    - moved font stylesheet to index.css
    - lazyload pages in McNutBanking.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,5 @@
-import React, { useEffect } from 'react';
-import Card from '@mui/material/Card';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography/Typography';
-import CardContent from '@mui/material/CardContent';
-import Cookies from 'js-cookie';
-import { PieChart, LineChart, BarChart, CurveType } from '@mui/x-charts';
-import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import 'react-virtualized/styles.css'; // only needs to be imported once
 
 export const Home = () => {
